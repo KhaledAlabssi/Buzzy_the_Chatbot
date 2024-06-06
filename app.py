a3fa77ec0bcdb956d12aab4a71659ef512dbf328
KhaledAlabssi <khaled.absi5@gmail.com>
2024-06-06 20:37:46 +0200

    tokenize and lemmatize data test commit

--- app.py
+++ app.py
@@ -14,16 +14,28 @@ classes = []
 documents = []
 
 
-# lemmatize & tokenize
+# tokenize
 
 for intent in data['intents']:
 
     for pattern in intent['patterns']:
 
-        wordList = nltk.word.tokenize(pattern)
+        wordList = nltk.word_tokenize(pattern)
         words.extend(wordList)
         documents.append((wordList, intent['tag']))
 
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
+
+# lemmatize
+
+ignore_words = ["?", "!", ".", ", "]
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+# print(f"words: {words}")
+# print(f"classes: {classes}")
+
 
