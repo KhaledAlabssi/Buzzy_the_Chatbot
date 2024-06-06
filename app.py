d4681c0dd21f16738014dacafed9547013ebec9a
KhaledAlabssi <khaled.absi5@gmail.com>
2024-06-06 20:31:05 +0200

    lemmatizing and tokenizing json data

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 import json
-
+import nltk
+from nltk.stem import WordNetLemmatizer
 
 # load data
 with open('intents.json') as file:
     data = json.load(file)
+
+lemmatizer = WordNetLemmatizer()
+
+# initialize lists
+words = []
+classes = []
+documents = []
+
+
+# lemmatize & tokenize
+
+for intent in data['intents']:
+
+    for pattern in intent['patterns']:
+
+        wordList = nltk.word.tokenize(pattern)
+        words.extend(wordList)
+        documents.append((wordList, intent['tag']))
+
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
